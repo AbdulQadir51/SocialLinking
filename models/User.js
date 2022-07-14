6f7a6a870b30885fb093cb503542cae62c9340b6
TahirMurtaza <abdulessa93@gmail.com>
2022-07-14 18:54:43 +0500

    User,Reaction,Thought models added

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is Required',
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+
+    ],
+    friends: [this]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
